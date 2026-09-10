7ac94f0dd1c19d904f41340cd5b02ff84cb49c58
izankhalissani39 <izankhalissani39@gmail.com>
2026-09-11 05:23:04 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // GitHub Pages project URL: https://izankhalissani39.github.io/Toko-MELIORA/
+  base: '/Toko-MELIORA/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
